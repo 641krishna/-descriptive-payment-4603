cf0034acd89ec69c5ce0789c98544fa381c6ee91
damanjot6767 <damanjot6767@gmail.com>
2022-11-09 14:22:26 +0530

    add in it

--- travelcity/src/App.js
+++ travelcity/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { HeaderInputs } from './Components/Carbook/HeaderInputs';
 function App() {
   return (
     <div>
-      
+      <HeaderInputs/>
     </div>
   );
 }
