c7caad31663c97bea3e4788a8e68949592f9fd40
Shrkrishna Aluri <shrikrishnaaluri641@gmail.com>
2022-11-12 13:58:11 +0530

    added

--- travelcity/src/Store/Store.jsx
+++ travelcity/src/Store/Store.jsx
@@ -3,7 +3,7 @@ import thunk from "redux-thunk"
 import { Authreducer } from "./Reducer";
 import {CarReducer} from "../Components/Carbook/CarRedux/Car.reducer"
 
+
+
 const Combine = combineReducers({Auth:Authreducer,Car:CarReducer});
-
-
 export const Store = legacy_createStore(Combine,applyMiddleware(thunk));
